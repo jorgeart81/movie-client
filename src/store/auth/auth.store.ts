1fd1a9489b54ce98f8f0b0d4a6f1b810dc55c174
Jorge Reyes <92123185+JorgeArt81@users.noreply.github.com>
2024-06-11 22:43:09 -0500

    auth.store login

--- src/store/auth/auth.store.ts
+++ src/store/auth/auth.store.ts
@@ -1,31 +1,50 @@
 import { create, StateCreator } from 'zustand';
 import { devtools, persist } from 'zustand/middleware';
 
-import { AuthStatus } from '../interfaces';
+import type { AuthStatus } from '../interfaces';
 import { SotorageKey, type User } from '@/models';
+import { AuthService } from '@/services';
 
 interface AuthState {
   user: User;
   status: AuthStatus;
   token?: string;
+  refreshToken?: string;
 }
 
-interface Actions {}
+interface Actions {
+  login: (email: string, password: string) => void;
+}
 
-const storeApi: StateCreator<
-  AuthState & Actions,
-  [['zustand/devtools', never]]
-> = set => ({
+const initialState: AuthState = {
   user: {
     id: '',
     name: '',
     email: '',
     role: '',
   },
-  status: 'unauthorized',
-  token: undefined,
+  status: 'authorized',
+};
+
+const storeApi: StateCreator<
+  AuthState & Actions,
+  [['zustand/devtools', never]]
+> = set => ({
+  ...initialState,
 
   // Actions
+  login: async (email: string, password: string) => {
+    try {
+      const { accessToken, refreshToken } = await AuthService.login({
+        email,
+        password,
+      });
+      set({ token: accessToken, refreshToken, status: 'authorized' });
+    } catch (error) {
+      set(initialState);
+      if (error instanceof Error) throw error.message;
+    }
+  },
 });
 
 export const useAuthStore = create<AuthState & Actions>()(
