c8333e3240f81bdecfc2ac60d30f8be5c7f41ebf
Jorge Reyes <92123185+JorgeArt81@users.noreply.github.com>
2024-06-14 22:01:07 -0500

    Create authLoader

---
+++ src/routes/loaders/auth.loader.tsx
@@ -0,0 +1,15 @@
+import { redirect } from 'react-router-dom';
+
+import { RoutePath } from '@/models';
+import { useAuthStore } from '@/store';
+
+export function authLoader() {
+  const status = useAuthStore.getState().status;
+  const statusValidate = useAuthStore.getState().statusValidate;
+
+  if (status !== 'authorized' || !statusValidate()) {
+    return redirect(RoutePath.ROOT);
+  }
+
+  return null;
+}
