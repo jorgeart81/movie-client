87edca3747db434cf9f731f882d24f05c38d56f2
Jorge Reyes <92123185+JorgeArt81@users.noreply.github.com>
2024-06-15 22:32:12 -0500

    Create cryptoAdapter

---
+++ src/utils/crypto.adapter.ts
@@ -0,0 +1,13 @@
+import CryptoJS from 'crypto-js';
+
+const secretKey = import.meta.env.VITE_CRYP_SECRETs;
+
+export const cryptoAdapter = {
+  encrypt: (data: string) => {
+    return CryptoJS.AES.encrypt(JSON.stringify(data), secretKey).toString();
+  },
+  decrypt: (encryptedData: string) => {
+    const bytes = CryptoJS.AES.decrypt(encryptedData, secretKey);
+    return JSON.parse(bytes.toString(CryptoJS.enc.Utf8));
+  },
+};
